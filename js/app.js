eac6fd19c13bb6a2fde03131ccc13b1d994cb280
Sam Bassett <samson.bassett@gmail.com>
2014-03-01 10:36:58 -0800

    clean up CDN error message

--- js/app.js
+++ js/app.js
@@ -3,10 +3,11 @@ App =
     init:function()
     {
         if(typeof Ext == 'undefined') 
-            alert('Ext.js undefined.  Site will not load correctly');
-           else
         {
-            console.log(Ext);
+            alert('Ext.js undefined.  Could not find CDN hosted at http://cdn.sencha.com/ext/gpl/4.2.1/  Site will not load correctly');
+        }
+        else
+        { 
             Ext.application(
             {
                 name: 'Lothrazar\'s Collection Tracker',
