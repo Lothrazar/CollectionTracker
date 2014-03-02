d2aa0fd1b6c6ac0cd52452ce54a6a4fb2e5ce364
Sam Bassett <samson.bassett@gmail.com>
2014-03-02 01:32:51 -0800

    trying to work with nodejs mysql

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ CollectionTracker
 =================
 
 Track my video game collection
+
+Everything in the /node_modules/mysql is from https://github.com/felixge/node-mysql
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,8 @@
--- TODO
+CREATE SCHEMA `tracker` ;
+ 
+
+CREATE TABLE `tracker`.`region` (
+  `region_id` INT NOT NULL DEFAULT 1,
+  `region_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`region_id`),
+  UNIQUE INDEX `region_id_UNIQUE` (`region_id` ASC));
