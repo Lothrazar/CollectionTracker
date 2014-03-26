fb8f0bd3204315746fc3db62357bad2dc68863cb
Sam Bassett <samson.bassett@gmail.com>
2014-03-26 00:22:27 -0700

    base window class

---
+++ js/base/sam.window.js
@@ -0,0 +1,18 @@
+Ext.define('Sam.window',
+{
+    extend:'Ext.window.Window' 
+    ,xtype:'swindow'
+    ,layout: 'fit' 
+    //,bodyPadding: 5
+    ,constructor:function(config)
+    {  
+      
+      	
+        this.callParent(arguments);//base class
+     }//end of constructor
+     
+     //override certain functions, then call the parent method
+     ,initComponent: function() {  this.callParent(arguments); } 
+  
+});//end of Ext.define
+  
